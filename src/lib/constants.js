4c39350b5c22fd8f6e4a325c2888e0374287cb5b
aadijain-glb <jainaadi91151@gmail.com>
2026-05-23 13:46:40 +0530

    Update label to 'Worked With Influencers'

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -12,7 +12,7 @@ export const AGENCY = {
 };
 
 export const STATS = [
-  { label: "Worked With", value: 200, suffix: "+" },
+  { label: "Worked With Influencers", value: 200, suffix: "+" },
   { label: "Campaign Reach", value: 100, suffix: "+" },
   { label: "Brand Collaborations", value: 50, suffix: "+" },
   { label: "Client Retention", value: 85, suffix: "%" },
